52ae90a7d65e6b0173999dbf24cc5b96de512979
fabiane9258 <fabiane9258@gmail.com>
2025-06-10 12:40:03 +0300

    Fix logical error in age classification condition for adults

--- age-classifier.py
+++ age-classifier.py
@@ -16,7 +16,7 @@ if age < 13:
     print("You are a child. ")
 elif age >= 13 and age < 17:
     print("You are a teenager. ")
-elif 18 >= age < 64:
+elif 18 <= age < 64:
     print("You are an adult. ")
 else:
     print("You are a senior citizen. ")
